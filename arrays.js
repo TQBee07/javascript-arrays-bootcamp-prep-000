ed04c1235e72843e006eb910605217025c97d7ec
tarinalee <tarinalee@gmail.com>
2019-01-28 03:11:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -42,8 +42,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
-  console.log(array)
+ return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
