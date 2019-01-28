b8d69423a9d776a2cf4a0b8bf9f7defcb59a9942
tarinalee <tarinalee@gmail.com>
2019-01-28 03:09:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,6 +38,7 @@ function accessElementInArray(array, index) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
